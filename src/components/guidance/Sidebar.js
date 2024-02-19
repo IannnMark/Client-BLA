b16d2490521a0181afa6c626b77d2a50a1ef8be1
ianmark8 <ianmarkmorga07@gmail.com>
2024-02-19 18:22:15 +0800

    no message

--- src/components/guidance/Sidebar.js
+++ src/components/guidance/Sidebar.js
@@ -1,49 +1,3 @@
-// import React from "react";
-
-// import { Link } from "react-router-dom";
-
-
-// const Sidebar = () => {
-//     return (
-//         <div className="sidebar-wrapper">
-//             <nav id="sidebar">
-//                 <ul className="list-unstyled components">
-
-
-//                     <li>
-//                         <a
-//                             href="#documentSubmenu"
-//                             data-toggle="collapse"
-//                             aria-expanded="false"
-//                             className="dropdown-toggle"
-//                         >
-//                             <i className="fa fa-file"></i> Violations
-//                         </a>
-
-//                         <ul className="collapse list-unstyled" id="documentSubmenu">
-//                             <li>
-//                                 <Link to="/guidance/violations">
-//                                     <i className="fa fa-clipboard"></i> All
-//                                 </Link>
-//                             </li>
-
-//                             <li>
-//                                 <Link to="/guidance/violation">
-//                                     <i className="fa fa-plus"></i> Create
-//                                 </Link>
-//                             </li>
-//                         </ul>
-//                     </li>
-
-
-//                 </ul>
-//             </nav>
-//         </div>
-//     );
-// };
-
-// export default Sidebar;
-
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -55,7 +9,6 @@ const GuidanceSidebar = () => {
         <div className="sidebar-wrapper">
             <nav id="sidebar">
                 <ul className="list-unstyled components">
-                    {/* Other sidebar items for guidance */}
                     {user && user.role === "guidance" && (
                         <li>
                             <Link to="/guidance/requests">
