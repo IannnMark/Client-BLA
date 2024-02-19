0cc63a466a6dce972c97f79455d6126c3724cfbd
ianmark8 <ianmarkmorga07@gmail.com>
2024-02-19 18:21:34 +0800

    add background images

--- src/components/document/Document.js
+++ src/components/document/Document.js
@@ -3,16 +3,13 @@ import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { addDocumentRequest } from '../../actions/requestActions';
 
-const Document = ({ document }) => {
+const Document = ({ document, backgroundImage }) => {
     const dispatch = useDispatch();
     const [quantity, setQuantity] = useState(1);
     const { user } = useSelector((state) => state.auth);
 
     useEffect(() => {
-        // Load the cart items from local storage if available
         const cartItems = JSON.parse(localStorage.getItem('cartItems')) || {};
-        console.log('Loaded Cart Items:', cartItems);
-
         const storedQuantity = cartItems[document._id] || 1;
         setQuantity(storedQuantity);
     }, [document._id]);
@@ -25,25 +22,19 @@ const Document = ({ document }) => {
         setQuantity((prevQty) => (prevQty > 1 ? prevQty - 1 : 1));
     };
 
-
     const addRequest = () => {
         dispatch(addDocumentRequest(document._id, quantity));
 
-        // Update the request documents in local storage
         const requestDocuments = JSON.parse(localStorage.getItem('requestDocuments')) || {};
         const updatedQuantity = parseInt(quantity, 10);
-
-        // Debugging: Log the request documents to check if they are correctly stored in local storage
-        console.log('Current request documents:', requestDocuments);
 
         requestDocuments[document._id] = updatedQuantity;
         localStorage.setItem('requestDocuments', JSON.stringify(requestDocuments));
     };
 
-
     return (
         <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-            <div className="card p-3 rounded">
+            <div className="card p-3 rounded" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/images/bglogin.jpg)`, backgroundSize: 'cover', padding: '20px' }}>
                 <img
                     className="card-img-top mx-auto"
                     src={document.images[0].url}
@@ -54,7 +45,6 @@ const Document = ({ document }) => {
                         <Link to={`document/${document._id}`}>{document.name}</Link>
                     </h5>
                     <p className="card-text">₱{document.price}</p>
-
 
                     {user ? (
                         <button
