1d7313cd6129c00c789501c1e4ad8eaba2f1bfe0
ianmark8 <ianmarkmorga07@gmail.com>
2024-04-09 20:07:58 +0800

    improve ui

--- src/components/admin/Sidebar.js
+++ src/components/admin/Sidebar.js
@@ -1,205 +1,99 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { Link } from "react-router-dom";
 
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Collapse from '@mui/material/Collapse';
-import BusinessIcon from '@mui/icons-material/Business';
-import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import ExpandLess from '@mui/icons-material/ExpandMore';
-import StorefrontIcon from '@mui/icons-material/Storefront';
-import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
-import NoteAddIcon from '@mui/icons-material/NoteAdd';
-import LocalMallIcon from '@mui/icons-material/LocalMall';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import PeopleIcon from '@mui/icons-material/People';
-import ReviewsIcon from '@mui/icons-material/Reviews';
-
 
 const Sidebar = () => {
-  const [open, setOpen] = useState(false);
-  const toggleDrawer = (open) => (event) => {
-    if (
-      event.type === 'keydown' &&
-      (event.key === 'Tab' || event.key === 'Shift')
-    ) {
-      return;
-    }
-
-    setOpen(open);
-  };
-
-  const [openDocuments, setOpenDocuments] = useState(false);
-
-  const handleDocumentsClick = () => {
-    setOpenDocuments(!openDocuments);
-  };
-
-  const [openProducts, setOpenProducts] = useState(false);
-
-  const handleProductsClick = () => {
-    setOpenProducts(!openProducts);
-  };
-
   return (
+    <div className="sidebar-wrapper">
+      <navv id="sidebar">
+        <ul className="list-unstyled components">
+          <li>
+            <Link to="/dashboard">
+              <i className="fa fa-tachometer"></i> Dashboard
+            </Link>
+          </li>
+          <li>
+            <a
+              href="#productSubmenu"
+              data-toggle="collapse"
+              aria-expanded="false"
+              className="custom-toggle"
+            >
+              <i className="fa fa-product-hunt"></i> Products
+            </a>
 
-    <>
-      <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        edge="start"
-        onClick={toggleDrawer(true)}
-        sx={{
-          marginRight: 2,
-          "& .MuiSvgIcon-root": {
-            fontSize: '3rem',
-            color: 'red',
-            marginLeft: '20px',
-          },
-        }}
-      >
-        <MenuIcon />
-        <p style={{ margin: "2px", fontWeight: "bold" }}>DASHBOARD</p>
-      </IconButton>
-      <Drawer anchor="left" open={open} onClose={toggleDrawer(false)}>
-        <List>
+            <ul className="collapse list-unstyled" id="productSubmenu">
+              <li>
+                <Link to="/admin/products">
+                  <i className="fa fa-clipboard"></i> List of Products
+                </Link>
+                <br />
+                <Link to="/admin/product">
+                  <i className="fa fa-plus"></i> Create
+                </Link>
+              </li>
+            </ul>
+          </li>
 
-          <ListItemButton component={Link} to="/dashboard">
-            <ListItemIcon>
+          <li>
+            <a
+              href="#documentSubmenu"
+              data-toggle="collapse"
+              aria-expanded="false"
+              className="custom-toggle"
+            >
+              <i className="fa fa-file"></i> Documents
+            </a>
 
-              <BusinessIcon />
-            </ListItemIcon>
-            <ListItemText primary="Dashboard" />
-          </ListItemButton>
+            <ul className="collapse list-unstyled" id="documentSubmenu">
+              <li>
+                <Link to="/admin/documents">
+                  <i className="fa fa-clipboard"></i> List of Documents
+                </Link>
+                <br />
+                <Link to="/admin/document">
+                  <i className="fa fa-plus"></i> Create
+                </Link>
+              </li>
+            </ul>
+          </li>
 
-          <ListItemButton onClick={handleDocumentsClick}>
-            <ListItemIcon>
-              <DocumentScannerIcon />
-            </ListItemIcon>
-            <ListItemText primary="Documents" />
-            {openDocuments ? <ExpandLess /> : <ExpandMore />}
-          </ListItemButton>
+          <li>
+            <Link to="/admin/orders">
+              <i className="fa fa-shopping-basket"></i> Orders
+            </Link>
+          </li>
 
+          <li>
+            <Link to="/admin/requests">
+              <i className="fa fa-file"></i> Requests
+            </Link>
+          </li>
 
-          <Collapse in={openDocuments} timeout="auto" unmountOnExit>
-            <List component="div" disablePadding>
-              <ListItemButton component={Link} to="/admin/documents" sx={{ pl: 4 }}>
+          <li>
+            <Link to="/admin/users">
+              <i className="fa fa-users"></i> Users
+            </Link>
+          </li>
 
-                <ListItemIcon>
-                  <DocumentScannerIcon />
-                </ListItemIcon>
-
-                <ListItemText primary="All" />
-              </ListItemButton>
-
-              <ListItemButton component={Link} to="/admin/document" sx={{ pl: 4 }}>
-
-                <ListItemIcon>
-                  <NoteAddIcon />
-                </ListItemIcon>
-
-                <ListItemText primary="Add Document" />
-              </ListItemButton>
-
-            </List>
-          </Collapse>
-
-
-          <ListItemButton component={Link} to="/admin/requests">
-            <ListItemIcon>
-
-              <VolunteerActivismIcon />
-            </ListItemIcon>
-            <ListItemText primary="Requests" />
-          </ListItemButton>
-
-
-
-          <ListItemButton onClick={handleProductsClick}>
-            <ListItemIcon>
-              <StorefrontIcon />
-            </ListItemIcon>
-            <ListItemText primary="Products" />
-            {openProducts ? <ExpandLess /> : <ExpandMore />}
-          </ListItemButton>
-          <Collapse in={openProducts} timeout="auto" unmountOnExit>
-            <List component="div" disablePadding>
-              <ListItemButton component={Link} to="/admin/products" sx={{ pl: 4 }}>
-
-                <ListItemIcon>
-                  <LocalMallIcon />
-                </ListItemIcon>
-
-                <ListItemText primary="All" />
-              </ListItemButton>
-
-              <ListItemButton component={Link} to="/admin/product" sx={{ pl: 4 }}>
-
-                <ListItemIcon>
-                  <AddCircleOutlineIcon />
-                </ListItemIcon>
-
-                <ListItemText primary="Add Product" />
-              </ListItemButton>
-
-            </List>
-          </Collapse>
+          <li>
+            <Link to="/admin/reviews">
+              <i className="fa fa-star"></i> Reviews
+            </Link>
+          </li>
 
 
-          <ListItemButton component={Link} to="/admin/orders">
-            <ListItemIcon>
-
-              <ShoppingBasketIcon />
-            </ListItemIcon>
-            <ListItemText primary="Orders" />
-          </ListItemButton>
-
-          <ListItemButton component={Link} to="/admin/users">
-            <ListItemIcon>
-
-              <PeopleIcon />
-            </ListItemIcon>
-            <ListItemText primary="Users" />
-          </ListItemButton>
+          <li>
+            <Link to="/admin/stock-history">
+              <i className="fa fa-star"></i> Stock History
+            </Link>
+          </li>
 
 
-          <ListItemButton component={Link} to="/admin/reviews">
-            <ListItemIcon>
-
-              <ReviewsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Reviews" />
-          </ListItemButton>
-
-
-
-          <ListItemButton component={Link} to="/admin/stock-history">
-            <ListItemIcon>
-
-              <ReviewsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Stock History" />
-          </ListItemButton>
-
-
-
-
-
-        </List>
-      </Drawer>
-    </>
-
-
-
-
+        </ul>
+      </navv>
+    </div>
   );
 };
 
