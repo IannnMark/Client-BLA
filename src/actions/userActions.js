93427ac7824fcd83170a57a6a08c9ae3f9ab8cf7
ianmark8 <ianmarkmorga07@gmail.com>
2024-05-03 21:15:51 +0800

    fix the user register

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -90,7 +90,9 @@ export const register = (userData) => async (dispatch) => {
       withCredentials: true,
     };
 
-    const { data } = await axios.post(`${process.env.REACT_APP_API}api/v1/register`, userData, config);
+    // const { data } = await axios.post(`${process.env.REACT_APP_API}api/v1/register`, userData, config);
+
+    const { data } = await axios.post(`${process.env.REACT_APP_API}/api/v1/admin/register`, userData, config);
 
     dispatch({
       type: REGISTER_USER_SUCCESS,
