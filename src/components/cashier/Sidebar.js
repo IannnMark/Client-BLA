bd331c98bbdd9ef95a270a093d10075bc389069f
ianmark8 <ianmarkmorga07@gmail.com>
2024-05-05 23:42:48 +0800

    fix sidebar

--- src/components/cashier/Sidebar.js
+++ src/components/cashier/Sidebar.js
@@ -1,50 +1,143 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "./sidebar.css"; // Import CSS file for sidebar styles
+
 
 const CashierSidebar = () => {
-    const user = useSelector((state) => state.auth.user);
+    const [open, setOpen] = useState(false);
+    const sidebarRef = useRef(null);
+
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+                setOpen(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    const toggleDropdown = (event) => {
+        event.preventDefault();
+        const dropdown = document.getElementById("documentDropdown");
+        dropdown.style.display =
+            dropdown.style.display === "none" ? "block" : "none";
+    };
+
+    const toggleDropdownn = (event) => {
+        event.preventDefault();
+        const dropdown = document.getElementById("productDropdown");
+        dropdown.style.display =
+            dropdown.style.display === "none" ? "block" : "none";
+    };
+
+    // const toggleDrawer = (open) => (event) => {
+    //     if (
+    //         event.type === 'keydown' &&
+    //         (event.key === 'Tab' || event.key === 'Shift')
+    //     ) {
+    //         return;
+    //     }
+
+    //     setOpen(open);
+    // };
+
+    // const [openBalances, setOpenBalances] = useState(false);
+
+    // const handleBalancesClick = () => {
+    //     setOpenBalances(!openBalances);
+    // };
 
     return (
-        <div className="sidebar-wrapper">
-            <navv id="sidebar">
-                <ul className="list-unstyled components">
+        <div className={`sidebar-container ${open ? "sidebar-open" : ""}`}>
+            {!open && (
+                <div
+                    className="sidebar-icon-button"
+                    onClick={toggleDrawer}
+                    style={{
+                        marginRight: "2px",
+                        cursor: "pointer",
+                    }}
+                >
+                    <FontAwesomeIcon
+                        icon={faBars}
+                        style={{
+                            marginRight: "5px",
+                            marginTop: "5px",
+                            fontSize: "3.5rem",
+                        }}
+                    />
+                    <p
+                        style={{ margin: "2px", fontWeight: "bold", fontSize: "2rem" }}
+                    ></p>
+                </div>
+            )}
+            {open && (
+                <>
+                    <div className="sidebar-backdrop" onClick={toggleDrawer}></div>
+                    <div
+                        className="custom-drawer"
+                        style={{
+                            backgroundColor: "#B1A078",
+                            marginTop: "-11px",
+                            zIndex: "1000",
+                        }}
+                        ref={sidebarRef}
+                    >
+                        <div className="drawer-content">
+                            <ul className="list-unstyled components">
+                                <li>
+                                    <a href="/cashier/requests">
+                                        <i class="fa-solid fa-envelope-open-text"></i> Requests
+                                    </a>
+                                </li>
 
-                    {user && user.role === "cashier" && (
-                        <li>
-                            <Link to="/cashier/requests">
-                                <i className="fa fa-file"></i> Requests
-                            </Link>
-                        </li>
-                    )}
+                                <li>
+                                    <a href="#" onClick={toggleDropdown}>
+                                        <i
+                                            className="fa fa-file"
+                                            style={{ marginBottom: "-0px" }}
+                                        ></i>{" "}
+                                        Balances
+                                    </a>
 
-                    <li>
-                        <a
-                            href="#documentSubmenu"
-                            data-toggle="collapse"
-                            aria-expanded="false"
-                            className="dropdown-toggle"
-                        >
-                            <i className="fa fa-file"></i> Balance
-                        </a>
-
-                        <ul className="collapse list-unstyled" id="documentSubmenu">
-                            <li>
-                                <Link to="/cashier/balances">
-                                    <i className="fa fa-clipboard"></i> All
-                                </Link>
-                                <br />
-                                <Link to="/cashier/balance">
-                                    <i className="fa fa-plus"></i> Create
-                                </Link>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </navv>
+                                    <ul
+                                        id="documentDropdown"
+                                        style={{ display: "none", marginTop: "15px" }}
+                                    >
+                                        <a href="/cashier/balances" className="document-link">
+                                            <i class="fa-solid fa-scale-balanced"></i> Balance List
+                                        </a>
+                                        <br></br>
+                                        <br></br>
+                                        <a href="/cashier/balance">
+                                            <i class="fa-solid fa-folder-plus"></i> Add Record
+                                        </a>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
+
+
+
+
     );
 };
 
 export default CashierSidebar;
-
